0308e15b64d60444dbda037365c18d1fcf932f8c
Gee Chow <chow@gebond.com>
2022-03-23 18:13:43 -0400

    commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NGINX Ingress Controller'
+copyright = '2022, NGINX'
+author = 'NGINX'
 
 release = '0.1'
 version = '0.1.0'
